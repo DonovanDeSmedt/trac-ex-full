2edd7da3c70b1ee3244c7bc6a7b5abef9bc5d85d
Steven Van Eenoo <steven.vaneenoo@delawareconsulting.com>
2016-11-17 16:14:06 +0100

    Add testing with jest

--- app/containers/home/home-reducers.js
+++ app/containers/home/home-reducers.js
@@ -2,11 +2,11 @@ import { handleActions } from 'redux-actions';
 import { combineReducers } from 'redux';
 import * as actionTypes from './constants';
 
-const items = handleActions({
+export const items = handleActions({
   [actionTypes.GET_ITEMS]: (state, action) => action.payload,
 }, []);
 
-const selectedIds = handleActions({
+export const selectedIds = handleActions({
   [actionTypes.TOGGLE_ITEM]: (state, action) => {
     if (state.includes(action.payload)) {
       return state.filter(itemId => itemId !== action.payload);
